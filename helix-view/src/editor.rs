9d04c1bbd732b4344def2149551e5eeb0fb3ee47
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-02-17 23:08:17 +0000

    feat: implement basic callback functions for prompt operarions

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -1062,6 +1062,7 @@ pub struct Editor {
     pub tree: Tree,
     pub next_document_id: DocumentId,
     pub documents: BTreeMap<DocumentId, Document>,
+    pub path_editing: Option<PathBuf>,
 
     // We Flatten<> to resolve the inner DocumentSavedEventFuture. For that we need a stream of streams, hence the Once<>.
     // https://stackoverflow.com/a/66875668
@@ -1250,6 +1251,7 @@ impl Editor {
             handlers,
             mouse_down_range: None,
             cursor_cache: CursorCache::default(),
+            path_editing: None,
         }
     }
 
