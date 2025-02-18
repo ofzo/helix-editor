b57e10bc961172c001bb7dc351d15299bb098fcf
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-02-18 16:49:00 +0000

    refactor: pass EditorData to callbacks, do not "compute" the root

--- helix-term/src/ui/picker.rs
+++ helix-term/src/ui/picker.rs
@@ -260,7 +260,7 @@ pub struct Picker<T: 'static + Send + Sync, D: 'static> {
     widths: Vec<Constraint>,
 
     callback_fn: PickerCallback<T>,
-    custom_key_handlers: PickerKeyHandlers<T>,
+    custom_key_handlers: PickerKeyHandlers<T, D>,
 
     pub truncate_start: bool,
     /// Caches paths to documents
@@ -396,7 +396,7 @@ impl<T: 'static + Send + Sync, D: 'static + Send + Sync> Picker<T, D> {
         }
     }
 
-    pub fn with_key_handlers(mut self, handlers: PickerKeyHandlers<T>) -> Self {
+    pub fn with_key_handlers(mut self, handlers: PickerKeyHandlers<T, D>) -> Self {
         self.custom_key_handlers = handlers;
         self
     }
@@ -527,7 +527,7 @@ impl<T: 'static + Send + Sync, D: 'static + Send + Sync> Picker<T, D> {
         if let (Some(callback), Some(selected)) =
             (self.custom_key_handlers.get(event), self.selection())
         {
-            callback(cx, selected, self.cursor);
+            callback(cx, selected, Arc::clone(&self.editor_data), self.cursor);
             EventResult::Consumed(None)
         } else {
             EventResult::Ignored(None)
@@ -1209,5 +1209,5 @@ impl<T: 'static + Send + Sync, D> Drop for Picker<T, D> {
 }
 
 type PickerCallback<T> = Box<dyn Fn(&mut Context, &T, Action)>;
-pub type PickerKeyHandler<T> = Box<dyn Fn(&mut Context, &T, u32) + 'static>;
-pub type PickerKeyHandlers<T> = HashMap<KeyEvent, PickerKeyHandler<T>>;
+pub type PickerKeyHandler<T, D> = Box<dyn Fn(&mut Context, &T, Arc<D>, u32) + 'static>;
+pub type PickerKeyHandlers<T, D> = HashMap<KeyEvent, PickerKeyHandler<T, D>>;
