a935edbc8d0e951442e774c41059235896180d5a
roadup <roadup.cc@gmail.com>
2025-03-29 11:46:26 +0800

    Merge from 'powerful-file-explorer'

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -1138,6 +1138,7 @@ pub struct Editor {
 
     pub mouse_down_range: Option<Range>,
     pub cursor_cache: CursorCache,
+    pub file_explorer_selected_path: Option<PathBuf>,
 }
 
 pub type Motion = Box<dyn Fn(&mut Editor)>;
@@ -1260,6 +1261,7 @@ impl Editor {
             handlers,
             mouse_down_range: None,
             cursor_cache: CursorCache::default(),
+            file_explorer_selected_path: None,
         }
     }
 
