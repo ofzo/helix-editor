173b16e06e3b1a1c6e381cf936050c557a23c3f9
skybe <roadup.cc@gmail.com>
2025-05-15 21:54:39 +0800

    fixup! fixup! fixup! add location for picker title

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -1798,7 +1798,6 @@ impl Editor {
                 true,
                 self.config.clone(),
                 self.syn_loader.clone(),
-                &self.diff_providers
                 &self.diff_providers,
             )?;
 
